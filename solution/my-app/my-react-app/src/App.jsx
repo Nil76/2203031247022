b58ae5c1b529b406fbcd6d63aa7385550206cfe6
Nilesh <mouryanilesh43@outlook.com>
2024-06-19 12:43:53 +0530

    first commit

---
+++ solution/my-app/my-react-app/src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        
+      </div>
+      <h1>E-comm Med</h1>
+      <div className="card">
+        <p>
+         
+        </p>
+      </div>
+      <p className="read-the-docs">
+        
+      </p>
+    </>
+  )
+}
+
+export default App
